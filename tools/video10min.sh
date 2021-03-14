e35a0e004b26fc587820b880c260b93a52de1188
claudioney <claudioney@gmail.com>
2021-03-13 23:48:54 -0300

    Bot 21 (#20)
    * BOT-17 comentando o sensor q parou de novo #criar_config_pra_sensores
    
    * BOT-18 comentando o metodo q ve a porta #criar_config_pra_sensores
    
    * BOT-19 criando config para sensores
    
    * BOT-20 adicionando shell script para gravas videos
    
    * BOT-21 adicionando while true no shell script
    
    Co-authored-by: Claudioney <claudioney@localhost.localdomain>

--- tools/video10min.sh
+++ tools/video10min.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
+
+i="0"
+while [ $i -lt 6]
+do
+rm /home/pi/camimage/$(date --date 'now - 6 hour' +%H)/directVideo*.mkv
 cd /home/pi/fontes/larabot
 cat ipcam.csv \
  | sed -e 's/\r$//g' \
- | awk -F, '{print "nohup ffmpeg -y -i rtsp://192.168.2."$2":1981//Master-0  -t 30 -codec copy /home/pi/camimage/HORA/directVideo_"$2"-TEMPO.mkv &"}' \
+ | awk -F, '{print "ffmpeg -y -i rtsp://192.168.2."$2":1981//Master-0  -t 600 -codec copy /home/pi/camimage/HORA/directVideo_"$2"-TEMPO.mkv"}' \
  | sed s/HORA/$(date +%H)/g \
  | sed s/TEMPO/$(date +%H-%M)/g \
  | sh
+done
 
 # removendo arquivos de 6 horas atras
 #rm /home/pi/camimage/$(date --date 'now - 6 hour' +%H)/directPhoto*.jpg
